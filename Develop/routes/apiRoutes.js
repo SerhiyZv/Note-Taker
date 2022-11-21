94dbcd7d3fda1828001c87b61ba49d7112861ff5
SerhiyZv <skiev31@gmail.com>
2022-11-20 21:40:01 -0500

    Update store, apiRoutes and htmlRoutes files

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,1 +1,33 @@
 const store = require('../db/store.js');
+
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => {
+
+        store
+        .getNote()
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => res.status(500).json(err));
+    });
+
+    app.post('/api/notes', (req, res) => {
+
+        store
+        .addNote(req.body)
+        .then((note) => {
+            return res.json(note);
+        })
+        .catch((err) => res.status(500).json(err));
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        
+        store
+        .removedNote(req.params.id)
+        .then(() => {
+            return res.json({ ok: true });
+        })
+        .catch((err) => res.status(500).json(err));
+    });
+};
