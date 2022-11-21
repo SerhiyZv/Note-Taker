2211bbb4fb55cae1ea196baeec728edd10356df4
SerhiyZv <skiev31@gmail.com>
2022-11-20 21:12:50 -0500

    add file store.js htmlRouter and apiRouter

---
+++ Develop/db/store.js
@@ -0,0 +1,53 @@
+const util = require('util');
+const fs = require('fs');
+
+const uid = require('uuid');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFileAsync('db/db.json', 'utf8');
+    }
+
+    write(note) {
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+    }
+
+    getNote() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+
+            return parsedNotes;
+        })
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+
+        if (!title || test) {
+            throw new Error("note 'title' and 'text' cannot be blank.");
+        }
+
+        const newNote = {title, text, if: uid() };
+
+        return this.getNotes()
+        .then((notes) => [...notes, newNote])
+        .then((updatedNotes) => this.write(updatedNotes));
+    }
+
+    removedNote(id) {
+        return this.getNotes()
+        .then((notes) => notes.filter((note) => note.id !== id))
+        .then((filteredNotes) => this.write(filteredNotes))
+    }
+}
+
+module.exports = new Store();
